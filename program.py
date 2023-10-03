a5841c9946d881d39ad6e01fa9769dcf6b411b13
Noah-Zwolak <144242513+Noah-Zwolak@users.noreply.github.com>
2023-10-03 13:24:55 +0000

    committed

--- program.py
+++ program.py
@@ -1,5 +1,9 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
+from zmq import HELLO_MSG
+
+
+print(HELLO_MSG world)
   # My Function
+  def myFunction():
