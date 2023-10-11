bbbeb1f5dfcba3b99bc3958ecc080e857acc5c9f
Noah-Zwolak <144242513+Noah-Zwolak@users.noreply.github.com>
2023-10-11 13:20:02 +0000

    commit

--- program.py
+++ program.py
@@ -1,6 +1,7 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-# My Function
-def myFunction():
-  
+  # My Function
+  def myFunction():
+    ("hello World")
+Print(myFunction)
