1f892bbc5367b21cf67e72485724ac88f217a645
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2023-10-11 09:13:49 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,9 +1,6 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-from zmq import HELLO_MSG
-
-
-print(HELLO_MSG world)
-  # My Function
-  def myFunction():
+# My Function
+def myFunction():
+  
